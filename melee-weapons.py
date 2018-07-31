fd3fe8b63bb0d43dbf84d389b7c56e8defa7e588
Brandon <bspagon@gmail.com>
2018-07-30 19:46:36 -0500

    Add attributes to MeleeWeapon class.

--- melee-weapons.py
+++ melee-weapons.py
@@ -1,3 +1,6 @@
-class MeleeWeapon(self, damage_low, damage_high):
-    def __init__(self, damage_low, damage_high):
-        pass
+class MeleeWeapon(self, damage_low, damage_high, damage_type):
+    def __init__(self, damage_low, damage_high, damage_type):
+        self.damage_low = damage_low
+        self.damage_high = damage_high
+        self.damage_type = damage_type
+        
